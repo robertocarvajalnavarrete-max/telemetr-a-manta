906780e3fde377be652968a3996161b3a6a2d4f7
robertocarvajalnavarrete-max <robertocarvajalnavarrete@gmail.com>
2026-06-22 16:11:52 -0300

    Update app_manta.py

--- app_manta.py
+++ app_manta.py
@@ -6,7 +6,7 @@ import io
 
 # --- CONFIGURACIÓN ---
 st.set_page_config(page_title="Control Manta - Centro de Ingeniería", layout="wide")
-SUPABASE_URL = "SUPABASE_URL = "https://pkpcbvobkgnrvqkpamun.supabase.co""
+SUPABASE_URL = "https://pkpcbvobkgnrvqkpamun.supabase.co"
 SUPABASE_KEY = "sb_publishable_8eE8fYlKG_Nl5864tYukKA_Cm9Fh2aV"
 COSTO_KWH = 0.15 # Ajusta según tu tarifa en CLP
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
